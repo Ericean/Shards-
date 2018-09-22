be6130f33573867abff786fea7f2066fa8fb1c12
Eric <Ericean@users.noreply.github.com>
2018-09-22 16:40:56 +0800

    Update cipher_problem.cpp

--- cipher_problem.cpp
+++ cipher_problem.cpp
@@ -2,14 +2,13 @@
 #include<cstdio>
 #include<cstring>
 #include<string>
-#include<vector>
+
 using namespace std;
-const int MAX = 201;
-char msg[MAX];
-char cipher[MAX];
+const int MAX = 250;
+char msg[MAX]={'\0'};
+char cipher[MAX]={'\0'};
 int keys[MAX];
 int numk;
-int inversions=0;
 
 int main()
 { 
@@ -20,21 +19,35 @@ int main()
 		for(int i= 0; i< numk; ++i){
 			cin >> k ;
 			keys[i]=k;
-			if(k!= i+1) ++inversions;
 		}
+		int cycle[MAX]={0};
+		//count cycles per element 
+		for(int i=0; i< numk; ++i){
+			//j= next position to go for ith element
+			int j= keys[i]-1;
+			while(keys[j]!= keys[i]){
+				cycle[i]++;
+				//next to next position to go for ith element
+				j = keys[j]-1;
+			}
+			cycle[i]++;
+		}
+		//for(int i=0; i< numk; ++i)
+		//	cout << cycle[i] <<" ";
+		//cout << endl;
 		//parse times;
 		int times; 
 		char c; 
 		while(cin >> times&& times!=0){
-			getchar();
-			gets(msg);
+			cin.ignore();
+			cin.getline(msg,MAX,'\n');
 			for(int k=strlen(msg); k< numk; ++k) msg[k] = ' ';
 			//cout << msg << " times: "<< times <<endl;
 			//encrypt the msg for times 
 			int j;
 			for(j=0; j< numk; ++j){
 				int idx = j; 
-				for(int i=0; i<	times%inversions; ++i){
+				for(int i=0; i<	times%cycle[j]; ++i){
 					idx = keys[idx] -1; 
 				}
 				cipher[idx] =msg[j];
